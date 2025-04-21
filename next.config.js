0ed6f33c602a033cf6a782a555b5d383fba770f7
Ebalin1411 <ebalinrosecse2006@gmail.com>
2025-04-21 20:31:28 +0530

    pages related push

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+  basePath: "/TotalQSR-Site", // Your repo name
+  assetPrefix: "/TotalQSR-Site", // Same here
+  trailingSlash: true, // Optional but useful for GitHub Pages
+  output: "export", // Enable static export
+};
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // next.config.js
+
+  basePath: "/TotalQSR-Site", // Your repo name
+  assetPrefix: "/TotalQSR-Site", // Same here
+  trailingSlash: true, // Optional but useful for GitHub Pages
+  output: "export", // Enable static export
 };
 
 export default nextConfig;
